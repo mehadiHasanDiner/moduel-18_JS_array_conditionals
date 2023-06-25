0af3919c4e3119a1b8d0402542484c9e5a0ca053
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-26 02:47:17 +0600

    push-pop.js git commit -m push-pop.js git commit -m push-pop.js

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
+
+// use push to add element to an array as the last element of the array
+numbers.push(35, 36, 37, 38, 39, 40, 41, 42, 43);
+// console.log(numbers);
+var friends = ["balam", "Kalam", "Salam", "Jalam"];
+friends.push("gelam");
+friends.push("pailam");
+// console.log(friends);
+
+// use pop to get the last element of the array
+// numbers.pop();
+var element = numbers.pop();
+
+console.log(element);
