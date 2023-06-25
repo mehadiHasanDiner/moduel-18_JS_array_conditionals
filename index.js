938bf6e801e9075b0291d256cf290c21102ac8ef
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-26 02:24:06 +0600

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var numbers = [45, 68, 89, 99];
+
+// 1. get element by index
+var element = numbers[1];
+// console.log(element);
+
+// 2. set element value by index
+numbers[1] = 62;
+numbers[3] = 100;
+console.log(numbers);
+
+// 3. find index of element
+var positionIndex = numbers.indexOf(89);
+var positionIndex = numbers.indexOf(123);
+console.log(positionIndex);
