533404fae9a731ec094276e10df47ac84211897d
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-26 02:02:54 +0600

    array types

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendAge = [11, 12, 13, 14, 15];
+var picnicFee = [2000, 3000, 4000, 5000];
+var niykas = ["mahi", "Opu", "Sabnoor", "Sabana"];
+var vowels = ["a", "e", "i", "o", "u"];
+
+console.log(picnicFee);
+console.log(niykas);
+console.log(friendAge.length);
